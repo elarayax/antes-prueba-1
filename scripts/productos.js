a78cbb40b098469382cb2905faa143fe5ffccbcd
elarayax <37583319+elarayax@users.noreply.github.com>
2026-09-02 21:19:06 -0400

    productos más ordenados

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,8 +1,8 @@
 let productoss = [
-  { id: 1, nombre: "algo 11111", precio: 5000 },
-  { id: 2, nombre: "algo 2", precio: 6000 },
-  { id: 3, nombre: "algo 3", precio: 7000 },
-  { id: 4, nombre: "algo 4", precio: 7000 }
+  { id: 1, img:"../images/algo.webp" , nombre: "algo 11111", precio: 5000 },
+  { id: 2, img:"../images/algo.webp", nombre: "algo 2", precio: 6000 },
+  { id: 3, img:"../images/algo.webp", nombre: "algo 3", precio: 7000 },
+  { id: 4, img:"../images/algo.webp", nombre: "algo 4", precio: 7000 }
 ];
 
 function mostrarProductos() {
@@ -11,6 +11,7 @@ function mostrarProductos() {
   for (const producto of productoss) {
     product.innerHTML += `
       <div class="caluga">
+        <img class="caluga-img" src="${producto.img}" alt="${producto.nombre}">
         <h2>${producto.nombre}</h2>
         <p>${producto.precio}</p>
       </div>
